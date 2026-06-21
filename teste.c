9d9dd48ce744322609d0736124ccaf473cdec17c
Vitor Melo <vitormelodev@gmail.com>
2026-06-21 12:51:15 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -43,6 +43,86 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
+char	*vm_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+	int		size;
+
+	if (!s2)
+		return (NULL);
+	size = 0;
+	if (s1)
+		size += ft_strlen(s1);
+	size += ft_strlen(s2);
+	str = malloc(size + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	if (s1)
+	{
+		while (s1[i])
+		{
+			str[i] = s1[i];
+			i++;
+		}
+	}
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (str);
+}
+
+char *get_rest(char *buf)
+{
+	char *str;
+	int i = 0;
+	while (buf[i] != '\0' && buf[i] != '\n')
+		i++;
+	if (buf[i] == '\0')
+		return (NULL);
+	if (buf[i] == '\n')
+		i++;
+	return (&buf[i]);
+}
+
+char	*extract_line(char *str)
+{
+	char *line;
+	int i = 0;
+
+	line = malloc (sizeof(char) * (ft_strlen(str) + 1));
+	while (str[i] != '\0' && str[i] != '\n')
+	{
+		line[i] = str[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char *get_next_line(int fd)
+{
+	int buffer_size = 10;
+	char static *line;
+	char *str;
+	char *to_re;
+	int n = 1;
+
+	str = malloc(sizeof(char) * (buffer_size + 1));
+	while (!ft_strchr(str, '\n') && n != 0)
+	{
+		n = read(fd, str, buffer_size);
+		str[n] = '\0';
+		line = vm_strjoin(line, str);
+	}
+	to_re = line;
+	line = get_rest(line);
+	return (extract_line(to_re));
+}
+
 int main(void)
 {
     int fd = open("Hello", O_RDONLY);
@@ -52,7 +132,7 @@ int main(void)
     printf(" - 1: %s\n", str);
     free(str);
 
-    /*str = get_next_line(fd);
+    str = get_next_line(fd);
     printf(" - 2: %s\n", str);
     free(str);
     
@@ -62,7 +142,7 @@ int main(void)
 
     str = get_next_line(fd);
     printf(" - 4: %s\n", str);
-    free(str);*/
+    free(str);
 
     close(fd);
     return 0;
